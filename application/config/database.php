cdb076d6fc79458d3c8cc5c1f132e16c7b6521f0
viranch <viranch.mehta@gmail.com>
2011-04-11 19:40:07 +0530

    Restore database config error

--- application/config/database.php
+++ application/config/database.php
@@ -43,7 +43,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = '123';
 $db['default']['database'] = 'academics_v3';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
